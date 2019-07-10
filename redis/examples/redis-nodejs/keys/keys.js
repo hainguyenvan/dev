7a65c7f71dfdd7bda05ee034a2bc0a8ef53269cf
hainv <hai213k57@gmail.com>
2019-07-10 17:39:55 +0700

    Add install redis with docker compose
    Signed-off-by: hainv <hai213k57@gmail.com>

---
+++ redis/examples/redis-nodejs/keys/keys.js
@@ -0,0 +1,25 @@
+const redis = require('redis');
+
+// redis.createClient(port, host);
+const client = redis.createClient();
+
+// get keys
+console.log('=== get value by key ===');
+let stringKey = 'stringKey';
+client.get(stringKey, function(error, result) {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    console.log('value: ', result);
+});
+
+// del key
+console.log('=== del key ===');
+client.del(stringKey, (err, result) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('del key: ', result);
+})
