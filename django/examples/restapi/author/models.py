0182a76245f58763cd7148b07815576957d80960
hainv <hai213k57@gmail.com>
2019-07-25 09:22:53 +0700

    Add uuid of id examples
    Signed-off-by: hainv <hai213k57@gmail.com>

--- django/examples/restapi/author/models.py
+++ django/examples/restapi/author/models.py
@@ -1,9 +1,9 @@
+import uuid
 from django.db import models
-
-# Create your models here.
 
 
 class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     email = models.EmailField()
 
